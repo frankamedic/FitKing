25c55995ef9c2cb6d282914226bb9b784645f0b4
Frank Sloan <frank.sloan@gmail.com>
2024-11-13 11:07:14 -0600

    Initial Commit

---
+++ StepKing/StepKingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepKingApp.swift
+//  StepKing
+//
+//  Created by Frank Sloan on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct StepKingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StepKingTests/StepKingTests.swift
@@ -0,0 +1,17 @@
+//
+//  StepKingTests.swift
+//  StepKingTests
+//
+//  Created by Frank Sloan on 11/13/24.
+//
+
+import Testing
+@testable import StepKing
+
+struct StepKingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
