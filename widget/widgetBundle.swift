9ab19a6d3f8fec773b7891cb737e8b6c994fd482
Frank Sloan <frank.sloan@gmail.com>
2024-11-13 14:56:09 -0600

    progress works

---
+++ widget/widgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by Frank Sloan on 11/13/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+        widgetControl()
+        widgetLiveActivity()
+    }
+}
